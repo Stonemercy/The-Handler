2c044a8d2222e3a633974400ebf06d7d11de5455
Lee <lee_pritchard@rocketmail.com>
2024-02-09 21:56:43 +0000

    0.8.4
    cogs\weather:
    - removed random crap
    - added data formatting to the WeatherData class that now edits the embeds before they're sent
    
    helpers\generators:
    - imported pyowm for formatting and math for the alert description
    - moved all the formatting from the weather cog to here in the form of functions
    - could possibly do this all in 1 function but {generic excuse}

--- cogs/weather.py
+++ cogs/weather.py
@@ -1,11 +1,8 @@
-import math
 from disnake.ext import commands, tasks
 from helpers.generators import Embeds, WeatherData
 from datetime import datetime
 from pyowm.owm import OWM
 from os import getenv
-from random import randint
-from asyncio import sleep
 from disnake import AppCmdInter
 
 # pyOWM docs: https://pyowm.readthedocs.io/en/latest/index.html
@@ -32,64 +29,15 @@ class Weather(commands.Cog):
             return
         channel = self.bot.get_channel(int(getenv("CHANNEL")))
         embeds = []
-        embed = Embeds.weather()
+        weather_embed = Embeds.weather()
         weather_call = weather_mgr.one_call(
             lat=HOME_LAT, lon=HOME_LON, exclude="minutely"
         )
-        weather_data = WeatherData(weather_call)
-        if weather_data.alerts is not None:
-            for i in weather_data.alerts:
-                alert_embed = Embeds.weather_alert()
-                alert_embed.add_field("Alert from:", i.sender, inline=False).add_field(
-                    "Alert name:", i.title, inline=False
-                ).add_field(
-                    "Time start:",
-                    f"<t:{i.start}:f>",
-                ).add_field(
-                    "Time end:",
-                    f"<t:{i.end}:f>",
-                ).add_field(
-                    "Description:", "", inline=False
-                )
-                descriptions = len(i.description) / 1024
-                bottom = 0
-                top = 1024
-                for j in range(math.ceil(descriptions)):
-                    alert_embed.add_field(
-                        "",
-                        i.description[bottom:top],
-                        inline=False,
-                    )
-                    bottom = top
-                    top = top + top
-                    continue
-            embeds.append(alert_embed)
-        embed.add_field(
-            "Sunrise:",
-            f"<t:{weather_data.current.sunrise_time(timeformat='unix')}:t>",
-        ).add_field(
-            "Sunset:",
-            f"<t:{weather_data.current.sunset_time(timeformat='unix')}:t>",
-        )
-        for hour in weather_data.hourly:
-            time = f"<t:{hour.reference_time(timeformat='unix')}:t>"
-            if not hour.snow:
-                snow = ""
-            else:
-                snow = f"Snow: `{hour.snow['1h']}mm/h`\n"
-            embed.add_field(
-                f"{time}\n{hour.detailed_status.title()}",
-                f"Temp: `{hour.temperature('celsius')['temp']:.0f}°`\n"
-                f"Feels like: `{hour.temperature('celsius')['feels_like']:.0f}°`\n"
-                f"Humidity: `{hour.humidity}%`\n"
-                f"Precipitation: `{hour.precipitation_probability:.0%}`\n"
-                f"{snow}"
-                f"Windspeed: `{hour.wind('miles_hour')['speed']:.0f}mph`\n"
-                f"Gusts: `{hour.wind('miles_hour')['gust']:.0f}mph`\n"
-                f"UV Index: `{hour.uvi}`",
-            )
-        embed.insert_field_at(2, "\u200b", "\u200b")
-        embeds.append(embed)
+        weather_data = WeatherData(weather_call, 3)
+        weather_data.add_weather_alerts(Embeds.weather_alert(), embeds)
+        weather_data.add_suns(weather_embed)
+        weather_data.add_weather(weather_embed)
+        embeds.append(weather_embed)
         await channel.send(embeds=embeds)
 
     @weather_info.before_loop
@@ -99,72 +47,15 @@ class Weather(commands.Cog):
     @commands.slash_command(description="Get weather for `x` amount of hours")
     async def weather(self, inter: AppCmdInter, hours: int = 3):
         embeds = []
-        embed = Embeds.weather()
-        if randint(1, 10000) == 10000:
-            embed.set_image(
-                "https://media0.giphy.com/media/J5q4qtKqQ4plPl4YJN/giphy.gif"
-            )
-            await inter.channel.send(embed=embed)
-            await sleep(3)
-            await inter.channel.send("haha, just kidding!")
-            embed.set_image("")
+        weather_embed = Embeds.weather()
         weather_call = weather_mgr.one_call(
             lat=HOME_LAT, lon=HOME_LON, exclude="minutely"
         )
-        weather_call = WeatherData(weather_call, hours)
-        if weather_call.alerts:
-            for i in weather_call.alerts:
-                alert_embed = Embeds.weather_alert()
-                alert_embed.add_field("Alert from:", i.sender, inline=False).add_field(
-                    "Alert name:", i.title, inline=False
-                ).add_field(
-                    "Time start:",
-                    f"<t:{i.start}:f>",
-                ).add_field(
-                    "Time end:",
-                    f"<t:{i.end}:f>",
-                ).add_field(
-                    "Description:", "", inline=False
-                )
-                descriptions = len(i.description) / 1024
-                bottom = 0
-                top = 1024
-                for j in range(math.ceil(descriptions)):
-                    alert_embed.add_field(
-                        "",
-                        i.description[bottom:top],
-                        inline=False,
-                    )
-                    bottom = top
-                    top = top + top
-                    continue
-            embeds.append(alert_embed)
-        embed.add_field(
-            "Sunrise:",
-            f"<t:{weather_call.current.sunrise_time(timeformat='unix')}:t>",
-        ).add_field(
-            "Sunset:",
-            f"<t:{weather_call.current.sunset_time(timeformat='unix')}:t>",
-        )
-        for hour in weather_call.hourly:
-            time = f"<t:{hour.reference_time(timeformat='unix')}:t>"
-            if not hour.snow:
-                snow = ""
-            else:
-                snow = f"Snow: `{hour.snow['1h']}mm/h`\n"
-            embed.add_field(
-                f"{time}\n{hour.detailed_status.title()}",
-                f"Temp: `{hour.temperature('celsius')['temp']:.0f}°`\n"
-                f"Feels like: `{hour.temperature('celsius')['feels_like']:.0f}°`\n"
-                f"Humidity: `{hour.humidity}%`\n"
-                f"Precipitation: `{hour.precipitation_probability:.0%}`\n"
-                f"{snow}"
-                f"Windspeed: `{hour.wind('miles_hour')['speed']:.0f}mph`\n"
-                f"Gusts: `{hour.wind('miles_hour')['gust']:.0f}mph`\n"
-                f"UV Index: `{hour.uvi}`",
-            )
-        embed.insert_field_at(2, "\u200b", "\u200b")
-        embeds.append(embed)
+        weather_data = WeatherData(weather_call, hours)
+        weather_data.add_weather_alerts(Embeds.weather_alert(), embeds)
+        weather_data.add_suns(weather_embed)
+        weather_data.add_weather(weather_embed)
+        embeds.append(weather_embed)
         await inter.send(embeds=embeds)
 
 
--- helpers/generators.py
+++ helpers/generators.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import disnake
+import math
+from pyowm.owm import weather_manager
 
 blue = disnake.Colour.blue()
 green = disnake.Colour.brand_green()
@@ -145,7 +147,67 @@ class Modals:
 
 
 class WeatherData:
-    def __init__(self, data, hours: int):
+    def __init__(self, data: weather_manager.one_call.OneCall, hours: int):
         self.alerts = data.national_weather_alerts
         self.current = data.current
         self.hourly = data.forecast_hourly[:hours]
+
+    def add_weather_alerts(
+        self, alert_embed: disnake.Embed, embeds_list: list
+    ) -> disnake.Embed:
+        if self.alerts is not None:
+            for i in self.alerts:
+                alert_embed.add_field("Alert from:", i.sender, inline=False).add_field(
+                    "Alert name:", i.title, inline=False
+                ).add_field(
+                    "Time start:",
+                    f"<t:{i.start}:f>",
+                ).add_field(
+                    "Time end:",
+                    f"<t:{i.end}:f>",
+                ).add_field(
+                    "Description:", "", inline=False
+                )
+                descriptions = len(i.description) / 1024
+                bottom = 0
+                top = 1024
+                for j in range(math.ceil(descriptions)):
+                    alert_embed.add_field(
+                        "",
+                        i.description[bottom:top],
+                        inline=False,
+                    )
+                    bottom = top
+                    top = top + top
+                    continue
+            embeds_list.append(alert_embed)
+
+    def add_suns(self, embed: disnake.Embed) -> disnake.Embed:
+        embed.add_field(
+            "Sunrise:",
+            f"<t:{self.current.sunrise_time(timeformat='unix')}:t>",
+        ).add_field(
+            "Sunset:",
+            f"<t:{self.current.sunset_time(timeformat='unix')}:t>",
+        ).add_field(
+            "\u200b", "\u200b"
+        )
+
+    def add_weather(self, embed: disnake.Embed) -> disnake.Embed:
+        for hour in self.hourly:
+            time = f"<t:{hour.reference_time(timeformat='unix')}:t>"
+            if not hour.snow:
+                snow = ""
+            else:
+                snow = f"Snow: `{hour.snow['1h']}mm/h`\n"
+            embed.add_field(
+                f"{time}\n{hour.detailed_status.title()}",
+                f"Temp: `{hour.temperature('celsius')['temp']:.0f}°`\n"
+                f"Feels like: `{hour.temperature('celsius')['feels_like']:.0f}°`\n"
+                f"Humidity: `{hour.humidity}%`\n"
+                f"Precipitation: `{hour.precipitation_probability:.0%}`\n"
+                f"{snow}"
+                f"Windspeed: `{hour.wind('miles_hour')['speed']:.0f}mph`\n"
+                f"Gusts: `{hour.wind('miles_hour')['gust']:.0f}mph`\n"
+                f"UV Index: `{hour.uvi}`",
+            )
